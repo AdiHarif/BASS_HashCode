9660b1fa5c4e48636ee349e0c2d910f194af3aed
baraveh <baraveh@gmail.com>
2021-02-25 20:35:12 +0200

    added street class

--- utils.h
+++ utils.h
@@ -5,9 +5,4 @@
 #ifndef BASS_HASHCODE_UTILS_H
 #define BASS_HASHCODE_UTILS_H
 
-// Data structure to store a graph edge
-struct Street {
-    int src, dest, weight, id;
-};
-
 #endif //BASS_HASHCODE_UTILS_H
