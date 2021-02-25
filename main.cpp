f131f392f1a2b2a00297802d0709259289ef5d17
Adi Harif <adi.harif@campus.technion.ac.il>
2021-02-25 22:19:27 +0200

    started implementing loading of files

---
+++ main.cpp
@@ -0,0 +1,36 @@
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <vector>
+
+#include <Street.h>
+#include <Car.h>
+
+using namespace std;
+
+void readStatsFiile(string path, int* stats){
+}
+
+vector<Street> readStreetsFile(string path){
+    ifstream in_file(path);
+    string line;
+    vector<Street> streets;
+
+    while (getline(in_file, line)){
+        istringstream iss(line);
+        streets.push_back(Street(iss));
+    }
+
+    return streets;
+}
+
+vector<Car> readCarFile(string path){
+    return vector<Car>();
+}
+
+
+int main() {
+    std::cout << "hello world" << std::endl;
+}
